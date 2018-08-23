8423f4b912caba2a60eb1b3d6edc86df82df7914
Patrick McGrath <patrick.mcgrath992@gmail.com>
2018-08-23 14:15:47 -0400

    s

--- src/main/java/com/techelevator/model/LoginFilter.java
+++ src/main/java/com/techelevator/model/LoginFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/*")
+@WebFilter("/*") //   /user/*
 public class LoginFilter implements Filter {
 
     @Override
